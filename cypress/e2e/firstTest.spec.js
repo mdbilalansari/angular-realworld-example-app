90fc066b80489455f947e4243de1b013daf27c6b
mdbilalansari <mdbilalansari1234@outlook.com>
2023-10-26 19:06:29 +0530

    Mocking API Response

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
 
+const { hasJSDocParameterTags } = require('typescript');
+
 describe('Test with backend', () => {
   beforeEach('login to the app', () => {
+    cy.intercept('GET', 'https://api.realworld.io/api/tags', { fixture: 'tags.json' });
     cy.loginToApplication();
   });
 
@@ -9,7 +12,7 @@ describe('Test with backend', () => {
     console.log('Login Succesful');
   });
 
-  it.only('verify correct request and response', () => {
+  it('verify correct request and response', () => {
     cy.intercept('POST', 'https://api.realworld.io/api/articles/').as('postArticles');
 
     cy.contains('New Article').click();
@@ -23,6 +26,35 @@ describe('Test with backend', () => {
       expect(xhr.response.statusCode).to.equal(201);
       expect(xhr.request.body.article.body).to.equal('This is a body of the Article.');
       expect(xhr.response.body.article.description).to.equal('Description of the Article');
+    });
+  });
+
+  it('verify popular tags are displayed', () => {
+    cy.get('.tag-list')
+      .should('contain', 'welcome')
+      .and('contain', 'My Tag')
+      .and('contain', 'Cypress')
+      .and('contain', 'Automation')
+      .and('contain', 'is')
+      .and('contain', 'Fun');
+  });
+
+  it.only('verify global feed likes count', () => {
+    cy.intercept('GET', 'https://api.realworld.io/api/articles/feed*', { articles: [], articlesCount: 0 });
+    cy.intercept('GET', 'https://api.realworld.io/api/articles*', { fixture: 'articles.json' });
+
+    cy.contains('Global Feed').click();
+    cy.get('app-article-list button').then((heartList) => {
+      expect(heartList[0]).to.contain('1');
+      expect(heartList[1]).to.contain('5');
+    });
+
+    cy.fixture('articles.json').then((file) => {
+      const articleLinnk = file.articles[1].slug;
+      file.articles[1].favoritesCount = 6;
+
+      cy.intercept('POST', 'https://api.realworld.io/api/articles/' + articleLinnk + '/favorite', file);
+      cy.get('app-article-list button').eq(1).click().should('contain', '6');
     });
   });
 });

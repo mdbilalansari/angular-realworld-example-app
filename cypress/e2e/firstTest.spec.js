464178964e83a073ae46ea584bc7631cf31dcc95
mdbilalansari <mdbilalansari1234@outlook.com>
2023-10-26 17:01:26 +0530

    Intercept an API

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,11 +1,28 @@
 /// <reference types="cypress" />
 
-describe("Test with backend", () => {
-  beforeEach("login to the app", () => {
+describe('Test with backend', () => {
+  beforeEach('login to the app', () => {
     cy.loginToApplication();
   });
 
-  it("Should log in", () => {
-    console.log("Login Succesful");
+  it('Should log in', () => {
+    console.log('Login Succesful');
+  });
+
+  it.only('verify correct request and response', () => {
+    cy.intercept('POST', 'https://api.realworld.io/api/articles/').as('postArticles');
+
+    cy.contains('New Article').click();
+    cy.get('[formcontrolname="title"]').type('Title of the Article');
+    cy.get('[formcontrolname="description"]').type('Description of the Article');
+    cy.get('[formcontrolname="body"]').type('This is a body of the Article.');
+    cy.contains('Publish Article').click();
+
+    cy.wait('@postArticles').then((xhr) => {
+      console.log(xhr);
+      expect(xhr.response.statusCode).to.equal(201);
+      expect(xhr.request.body.article.body).to.equal('This is a body of the Article.');
+      expect(xhr.response.body.article.description).to.equal('Description of the Article');
+    });
   });
 });
